7257c7afe7372b9f5d35f283f92b082c6cfe7e14
huydinh-uit <19520589@gm.uit.edu.vn>
2023-05-26 11:13:22 +0700

    add @GetMapping "confirm"

--- src/main/java/huydinh/login_registration/Controller/RegistrationController.java
+++ src/main/java/huydinh/login_registration/Controller/RegistrationController.java
@@ -3,10 +3,7 @@ package huydinh.login_registration.Controller;
 import huydinh.login_registration.Entity.AppUser.RegistrationRequest;
 import huydinh.login_registration.Service.RegistrationService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/registration")
@@ -18,5 +15,10 @@ public class RegistrationController {
     @PostMapping
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
+    }
+
+    @GetMapping(path = "confirm")
+    public String confirm(@RequestParam("token") String token){
+        return registrationService.confirmToken(token);
     }
 }
